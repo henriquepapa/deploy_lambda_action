d33292911f616b403836152962065c6a84bc8e19
Henrique Papa <henrique_ahg@hotmail.com>
2021-10-18 16:13:35 -0300

    sonnar in deploy

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('O Papa é Pop!')
+        'body': json.dumps('Test SonarCloud')
     }
